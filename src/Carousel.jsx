90911985d34adfac27100eef9e215de5af8e91e5
franko <fstipanov@gmail.com>
2023-05-03 08:53:30 +0200

    transform:translateX(100%) without overflow: hidden

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -7,6 +7,7 @@ const Carousel = () => {
   // list of people - we have all people on the top of each other
   const [people, setPeople] = useState(list);
 
+  // for logic we will use css transform(100%)
   const prevSlide = () => {
     console.log('prev');
   };
@@ -19,21 +20,21 @@ const Carousel = () => {
       {people.map((person) => {
         const { id, image, name, title, quote } = person;
         return (
-          <article key={id} className="slide">
+          <article key={id} className="slide next-slide">
             <img className="person-img" src={image} alt={name} />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
             <p className="text">{quote}</p>
             <FaQuoteRight className="icon" />
-            <button type="button" className="prev" onClick={prevSlide}>
-              <FaAngleLeft />
-            </button>
-            <button type="button" className="next" onClick={nextSlide}>
-              <FaAngleRight />
-            </button>
           </article>
         );
       })}
+      <button type="button" className="prev" onClick={prevSlide}>
+        <FaAngleLeft />
+      </button>
+      <button type="button" className="next" onClick={nextSlide}>
+        <FaAngleRight />
+      </button>
     </section>
   );
 };
