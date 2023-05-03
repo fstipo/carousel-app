931d9fc40eae3896d70c1d713ec6b8caa3a2267e
franko <fstipanov@gmail.com>
2023-05-03 10:51:46 +0200

    logic works

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -6,21 +6,27 @@ const Carousel = () => {
   // const [people, setPeople] = useState(shortList);
   // list of people - we have all people on the top of each other
   const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
 
   // for logic we will use css transform(100%)
-  const prevSlide = () => {
-    console.log('prev');
-  };
-  const nextSlide = () => {
-    console.log('next');
-  };
+  const prevSlide = () =>
+    setCurrentPerson((prev) => (prev - 1 + people.length) % people.length);
+
+  const nextSlide = () =>
+    setCurrentPerson((prev) => (prev + 1) % people.length);
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
         return (
-          <article key={id} className="slide next-slide">
+          <article
+            key={id}
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+            }}
+          >
             <img className="person-img" src={image} alt={name} />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
@@ -29,7 +35,11 @@ const Carousel = () => {
           </article>
         );
       })}
-      <button type="button" className="prev" onClick={prevSlide}>
+      <button
+        type="button"
+        className="prev"
+        onClick={() => prevSlide(currentPerson)}
+      >
         <FaAngleLeft />
       </button>
       <button type="button" className="next" onClick={nextSlide}>
