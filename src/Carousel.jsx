93f3c14ba6807a74b14ad9eafe6e5edea887f2f9
franko <fstipanov@gmail.com>
2023-05-03 08:43:52 +0200

    first commit :rocket:

---
+++ src/Carousel.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { shortList, list, longList } from './data';
+import { FaQuoteRight, FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+
+  const prevSlide = () => {
+    console.log('prev');
+  };
+  const nextSlide = () => {
+    console.log('next');
+  };
+
+  return (
+    <section className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article key={id} className="slide">
+            <img className="person-img" src={image} alt={name} />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+            <button type="button" className="prev" onClick={prevSlide}>
+              <FaAngleLeft />
+            </button>
+            <button type="button" className="next" onClick={nextSlide}>
+              <FaAngleRight />
+            </button>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+export default Carousel;
